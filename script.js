6fcd248327562146766e827472a6c107a6de9269
Ahmet Vatansever <ahmetvatansever49@gmail.com>
2024-02-18 23:40:57 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
 
-function getRandomQuote()
-{
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    let correct = true;
+    const arrayValue = quoteInputElement.value.split('');
+    arrayQuote.forEach((characterSpan, index) => {
+        
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        }
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        }
+        else {
+            characterSpan.classList.add('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
+        }
 
+    }
+    )
+    if (correct === true) { renderNewQuote(); }
+})
+
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(res => res.json())
+        .then(data => data.content)
 }
+
+async function renderNewQuote() {
+    const quote = await getRandomQuote();
+    quoteDisplayElement.innerHTML = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    });
+
+
+    quoteInputElement.value = null;
+}
+renderNewQuote();
