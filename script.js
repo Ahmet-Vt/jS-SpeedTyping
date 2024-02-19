8e16f72716fd584413383ac02959d87e20e8a17c
Ahmet Vatansever <ahmetvatansever49@gmail.com>
2024-02-19 17:00:51 +0300

    I have added counter of time and sentence. 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
-
+const counter = document.getElementById('counter');
+const timerElement = document.getElementById('timer');
+let remainingTime = 90;
+let count = 0;
+const sure = "time";
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span');
     let correct = true;
@@ -26,16 +30,18 @@ quoteInputElement.addEventListener('input', () => {
 
     }
     )
-    if (correct === true) { renderNewQuote(); }
+    if (correct === true) 
+    { 
+        count++;
+        renderNewQuote();
+        counter.textContent = count ;
+    }
 })
-
-
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
         .then(res => res.json())
         .then(data => data.content)
 }
-
 async function renderNewQuote() {
     const quote = await getRandomQuote();
     quoteDisplayElement.innerHTML = '';
@@ -43,9 +49,25 @@ async function renderNewQuote() {
         const characterSpan = document.createElement('span');
         characterSpan.innerText = character;
         quoteDisplayElement.appendChild(characterSpan);
+        
     });
-
-
+    
     quoteInputElement.value = null;
+    startTimer();
 }
+
+let startTime;
+function startTimer()
+{
+    timerElement.innerText = 0;
+    startTime = new Date();
+    setInterval(()=>{
+        timer.innerText = getTimerTime();
+    },1000)
+}
+function getTimerTime()
+{
+   return Math.floor((new Date() - startTime)/1000);
+}
+startTimer();
 renderNewQuote();
